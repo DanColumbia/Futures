8b7392d5f95e4db76d6e0b39a5db692fb1e5aaef
dx <dx@zestfinance.com>
2015-10-05 21:19:43 +0000

    10/5 commit

--- targetGenerator.R
+++ targetGenerator.R
@@ -1,9 +1,10 @@
 library(data.table)
 library(TTR)
-targetGenerator <- function(priceSeries, executionWindow = 1, holdingPeriod = 5)
+targetGenerator <- function(priceSeries, executionWindow = 1, fixedHoldingPeriod = FALSE, holdingPeriod = 5)
 {
+    # fixedHoldingPeriod = FALSE to exit trade on next reverse signal, = TRUE to exit after period of holdingPeriod
     ps <- priceSeries
     ps[,curTimeIndex:=time(dateTime)]
-    ps$inPrice <- rollapply(ps$index, executionWindow, mean)
-    ps$outPrice <- 
+    ps$executionPrice <- rollapply(ps$index, executionWindow, mean)
+    ps$profit <- 
 }
