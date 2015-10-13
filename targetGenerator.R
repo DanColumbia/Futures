6d824256ad80b90bb6e6eb579be5362642cfb29d
dx <dx@zestfinance.com>
2015-10-13 05:12:15 +0000

    1012 commit

--- targetGenerator.R
+++ targetGenerator.R
@@ -1,10 +1,25 @@
 library(data.table)
 library(TTR)
-targetGenerator <- function(priceSeries, executionWindow = 1, fixedHoldingPeriod = FALSE, holdingPeriod = 5)
+library(dplyr)
+library(zoo)
+targetGenerator <- function(priceSeries, executionWindow = 1, fixedHoldingPeriod = TRUE, holdingPeriod = 5)
 {
     # fixedHoldingPeriod = FALSE to exit trade on next reverse signal, = TRUE to exit after period of holdingPeriod
     ps <- priceSeries
-    ps[,curTimeIndex:=time(dateTime)]
+
+    ps[,date:=as.Date(dateTime)]
+    
+    # get time point ranking within each day
+    ps[,timeIndex:=time(dateTime)]
+    ps <- cbind(ps, timeInDay=ave(ps$timeIndex, ps$date, FUN=rank))
+    ps[,timeIndex:=NULL]
+    ps[,timeInDay:=as.numeric(timeInDay)]
+
     ps$executionPrice <- rollapply(ps$index, executionWindow, mean)
-    ps$profit <- 
+    if(fixedHoldingPeriod){# sell at # of holdingPeriod days later, in the morning? (WANT TO IMPLEMENT IN THE MORNING, BUT HOW???)
+        ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16) - ps$executionPrice)/ps$executionPrice
+    }else{
+        ps$profit <- 
+    }
 }
+
