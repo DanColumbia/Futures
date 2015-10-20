21b252e81db528d3bf85355fe7c273f177f09c14
dx <dx@zestfinance.com>
2015-10-20 15:53:12 +0000

    1020 commit

--- targetGenerator.R
+++ targetGenerator.R
@@ -16,10 +16,72 @@ targetGenerator <- function(priceSeries, executionWindow = 1, fixedHoldingPeriod
     ps[,timeInDay:=as.numeric(timeInDay)]
 
     ps$executionPrice <- rollapply(ps$index, executionWindow, mean)
-    if(fixedHoldingPeriod){# sell at # of holdingPeriod days later, in the morning? (WANT TO IMPLEMENT IN THE MORNING, BUT HOW???)
-        ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16) - ps$executionPrice)/ps$executionPrice
+    if(fixedHoldingPeriod){# sell at # of holdingPeriod days later, in the morning only
+        ps$test<-ifelse(ps$timeInDay==1,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==2,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-1) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==3,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-2) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==4,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-3) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==5,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-4) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==6,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-5) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==7,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-6) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==8,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-7) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==9,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-8) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==10,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-9) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==11,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-10) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==12,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-11) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==13,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-12) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==14,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-13) - ps$executionPrice)/ps$executionPrice,
+                    ifelse(ps$timeInDay==15,ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-14) - ps$executionPrice)/ps$executionPrice,
+                    ps$profit <- 100*(lead(ps$executionPrice, holdingPeriod*16-15) - ps$executionPrice)/ps$executionPrice)
+        ))))))))))))))
     }else{
         ps$profit <- 
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ps$exitTest<-ifelse(ps$timeInDay==1,ps$profit <- lead(ps$executionPrice, holdingPeriod*16),
+# ifelse(ps$timeInDay==2,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-1),
+# ifelse(ps$timeInDay==3,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-2),
+# ifelse(ps$timeInDay==4,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-3),
+# ifelse(ps$timeInDay==5,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-4),
+# ifelse(ps$timeInDay==6,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-5),
+# ifelse(ps$timeInDay==7,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-6),
+# ifelse(ps$timeInDay==8,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-7),
+# ifelse(ps$timeInDay==9,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-8),
+# ifelse(ps$timeInDay==10,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-9),
+# ifelse(ps$timeInDay==11,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-10),
+# ifelse(ps$timeInDay==12,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-11),
+# ifelse(ps$timeInDay==13,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-12),
+# ifelse(ps$timeInDay==14,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-13),
+# ifelse(ps$timeInDay==15,ps$profit <- lead(ps$executionPrice, holdingPeriod*16-14),
+# ps$profit <- lead(ps$executionPrice, holdingPeriod*16-15)
+# )))))))))))))))
